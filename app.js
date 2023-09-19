bbd3cfc5fca59e0b40a6ca238cb9036f0004f7be
jinn <gumingcn@gmail.com>
2023-09-19 14:24:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Argo CD!'))
+app.get('/', (req, res) => res.send('Hello Argo CD 0919!'))
 app.listen(3000, () => console.log('Server ready'))
